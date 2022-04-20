7e57f0ab3dec2c17c5ed981a3400f1c2ccaeca6c
Edgar A. Dikenge <dikengeofficial@gmail.com>
2022-04-20 02:50:01 +0100

    removing and set the global css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-import FirstComponent from './components/FirstComponent'
-import Hooks from './components/Hooks';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <FirstComponent/>
-        <hr/>
-        <Hooks/>
-      </header>
-
-
+      <h1>Tasks</h1>
     </div>
   );
 }
